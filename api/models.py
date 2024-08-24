05982e26ccd78e28f888e017d627535fc2bc363f
coco483 <151052125+coco483@users.noreply.github.com>
2024-08-24 18:20:44 +0900

    [model] model modification, admin page setup

--- api/models.py
+++ api/models.py
@@ -7,14 +7,10 @@ class User(models.Model):
     id = models.CharField(primary_key=True, max_length=50)
     goat_age = models.IntegerField(default=0)
     grass_num = models.IntegerField(default=0)
-    garden_array = ArrayField(models.BooleanField(max_length=1), size=9, blank=True, null=True) 
+    garden_array = ArrayField(models.BooleanField(max_length=1), size=81, default=[False]*81) 
     #여기서부터 optional
-    subscription_end_date = models.DateField(null=True)
+    subscription_end_date = models.DateField(null=True, blank=True)
     donated_goat_num = models.IntegerField(default=0)
-    def save(self, *args, **kwargs):
-        if not self.garden_array:  # If the field is empty or None
-            self.garden_array = [0] * 9  # Set to an array of None
-        super(User, self).save(*args, **kwargs)
 
 class Donation(models.Model):
     donor = models.ForeignKey("User", on_delete=models.CASCADE)
